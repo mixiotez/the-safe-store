538574c86b999ac8c867ae4c90402e2d3cd39326
mikempala <miguel.tellez1997@gmail.com>
2020-06-20 21:35:25 -0500

    Added QR code and styles to main page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'The Safe Store' });
 });
 
 router.get('/privacy', (req, res) => {
-  res.render('privacyPolicy', { title: 'Privacy Policy' });
+  res.render('privacyPolicy', { title: 'The Safe Store - Privacy Policy' });
 });
 
 module.exports = router;
